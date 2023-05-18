e962df5a6f41c649485dd0097e3357857a9ef95f
baddaction <124236086+baddaction@users.noreply.github.com>
2023-05-17 20:40:03 -0600

    mejoras
    arreglo de pagina, index nav y buscador junto con su css

---
+++ Pagina/Scripts.js
@@ -0,0 +1,8 @@
+function mostrarMenu() {
+    document.querySelector("#navbarSupportedContent").classList.add("show");
+  }
+  
+  function ocultarMenu() {
+    document.querySelector("#navbarSupportedContent").classList.remove("show");
+  }
+  
