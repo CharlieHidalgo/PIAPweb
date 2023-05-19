4c5c260b128055db83a546e1d29a8116ccc4eb2f
baddaction <124236086+baddaction@users.noreply.github.com>
2023-05-19 06:33:24 -0600

    Mejoras
    Mejoro boton ventas y articulos junto con un banner abajo para anuncios animados

--- Pagina/Scripts.js
+++
@@ -1,8 +0,0 @@
-function mostrarMenu() {
-    document.querySelector("#navbarSupportedContent").classList.add("show");
-  }
-  
-  function ocultarMenu() {
-    document.querySelector("#navbarSupportedContent").classList.remove("show");
-  }
-  
